7f8eb432e3ba66ef1a6668be2c6ce14b6feb392b
johanesteves <jsesteves91@gmail.com>
2017-12-28 21:56:37 -0500

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
 
-class Filter extends Component {
-  constructor() {
-    super();
+const Filter = function (props) {
+  return(
+    <select onChange={props.handleChange} defaultValue='all'>
+      <option value='all'>All</option>
+      {props.filters.map(filter =>
+        <option key={filter} value={filter}>{filter}</option>
+      )}
+    </select>
+  )
+};
 
-    this.state = {
-      filters: []
-    };
-  }
+Filter.defaultProps = {
+  filters: [],
+  handleChange: undefined
+};
 
-  componentWillMount() {
-    this.fetchFilters();
-  }
 
-  fetchFilters = () => {
-    fetch('/api/fruit_types')
-      .then(response => response.json())
-      .then(filters => this.setState({ filters }));
-  }
-
-  render() {
-    return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
-        <option value='all'>All</option>
-        {this.state.filters.map(filter =>
-          <option key={filter} value={filter}>{filter}</option>
-        )}
-      </select>
-    );
-  }
-}
+//
+// class Filter extends Component {
+//   constructor() {
+//     super();
+//
+//     this.state = {
+//       filters: []
+//     };
+//   }
+//
+//   componentWillMount() {
+//     this.fetchFilters();
+//   }
+//
+//   fetchFilters = () => {
+//     fetch('/api/fruit_types')
+//       .then(response => response.json())
+//       .then(filters => this.setState({ filters }));
+//   }
+//
+//   render() {
+//     return (
+//       <select onChange={this.props.handleChange} defaultValue='all'>
+//         <option value='all'>All</option>
+//         {this.state.filters.map(filter =>
+//           <option key={filter} value={filter}>{filter}</option>
+//         )}
+//       </select>
+//     );
+//   }
+// }
 
 export default Filter;
